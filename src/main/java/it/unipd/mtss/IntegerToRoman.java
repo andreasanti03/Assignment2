ae2f8d0103ed1b6a6c47e94a4503593024627ed7
Francesco Pozzobon <francesco.pozzobon.1@studenti.unipd.it>
2024-05-14 12:38:07 +0200

    close #10; test e funzionalità per convertire i numeri da 1 a 3

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,4 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// ANDREA SANTI 2084624
+// FRANCESCO POZZOBON 2075513
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+    public static String convert(int number) {
+        if(number == 1){
+            return "I";
+        }else if(number == 2){
+            return "II";
+        }else {
+            return "III";
+        }
+    }
 }
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,4 +1,22 @@
+////////////////////////////////////////////////////////////////////
+// ANDREA SANTI 2084624
+// FRANCESCO POZZOBON 2075513
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IntegerToRomanTest {
+    @Test
+    public void evaluatePrimiTre(){
+        String variabileOne = "I";
+        String variabileTwo = "II";
+        String variabileThree = "III";
+        assertEquals(variabileOne, IntegerToRoman.convert(1));
+        assertEquals(variabileTwo, IntegerToRoman.convert(2));
+        assertEquals(variabileThree, IntegerToRoman.convert(3));
+    }
 }
