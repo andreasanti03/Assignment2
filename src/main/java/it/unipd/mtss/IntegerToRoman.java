83777e75d913515b7c27ff22baa926ead128937a
Andrea <andrea.santi.5@studenti.unipd.it>
2024-05-14 15:25:24 +0200

    close #14; Implementati test e funzionalità per la conversione dei primi 50 numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,4 +1,35 @@
+////////////////////////////////////////////////////////////////////
+// ANDREA SANTI 2084624
+// FRANCESCO POZZOBON 2075513
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+    public static String convert(int number) {
+
+        String[] decine = {"","X","XX","XXX","XL","L"};
+        String[] unita = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+
+
+        char[] input = String.valueOf(number).toCharArray();
+
+        int posdecine = -1;
+        int posunita = -1;
+
+        if(input.length==2){
+            posdecine = input[0] - '0';
+            posunita = input[1] - '0';
+        }else if(input.length==1){
+            posunita = input[0] - '0';
+        }
+
+        if(posdecine!=-1){
+            return decine[posdecine]+unita[posunita];
+        }
+        else{
+            return unita[posunita];
+        }
+
+    }
 }
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,4 +1,33 @@
+////////////////////////////////////////////////////////////////////
+// ANDREA SANTI 2084624
+// FRANCESCO POZZOBON 2075513
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IntegerToRomanTest {
+    @Test
+    public void evalutePrimiCinquanta(){
+        String variabileTwentuOne = "XXI";
+        String variabileTwentyNine = "XXIX";
+        String variabileThirtyFour = "XXXIV";
+        String variabileThirtySix = "XXXVI";
+        String variabileFourty = "XL";
+        String variabileFourtyFive = "XLV";
+        String variabileFourtyNine = "XLIX";
+        String variabileFifty = "L";
+        assertEquals(variabileTwentuOne, IntegerToRoman.convert(21));
+        assertEquals(variabileTwentyNine, IntegerToRoman.convert(29));
+        assertEquals(variabileThirtyFour, IntegerToRoman.convert(34));
+        assertEquals(variabileThirtySix, IntegerToRoman.convert(36));
+        assertEquals(variabileFourty, IntegerToRoman.convert(40));
+        assertEquals(variabileFourtyFive, IntegerToRoman.convert(45));
+        assertEquals(variabileFourtyNine, IntegerToRoman.convert(49));
+        assertEquals(variabileFifty, IntegerToRoman.convert(50));
+    }
+
 }
