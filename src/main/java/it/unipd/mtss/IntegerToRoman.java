fd3482de17d8abf2ea03438ee7b7125a444a7683
Andrea <andrea.santi.5@studenti.unipd.it>
2024-05-14 16:32:24 +0200

    close #16; Implementati test e funzionalità per la conversione dei primi 500 numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -9,7 +9,7 @@ public class IntegerToRoman {
     public static String convert(int number) {
 
         String[] centinaia = {"","C","CC","CCC","CD","D"};
-        String[] decine = {"","X","XX","XXX","XL","L"};
+        String[] decine = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
         String[] unita = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
 
 
